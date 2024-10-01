92fdc9433e846c041831649f56c7006188b0688f
jaydipjadhav <jaydipdjadhav>
2024-10-01 19:22:34 +0530

    flatList

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,10 @@
-import { ScrollView, Text, View } from "react-native";
+import { FlatList, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <>   
    <ScrollView indicatorStyle="white" horizontal contentContainerStyle={{
     marginTop : 30
-   }} >
-    {
-      [1,2,3,4,5,6,7,89,9].map((item) =>{
-        return(
-          <View
-          style={{
-            width :100,
-            height : 100,
-            backgroundColor : "white",
-            marginBottom :20,
-            marginRight : 20,
-            borderRadius : 50,
-            borderWidth  :1
-          }}
-          />
-        )
-      })
-    }
-   </ScrollView>
-
-
-   <ScrollView indicatorStyle="white" horizontal contentContainerStyle={{
-    marginTop : 30,
-    flex : 1,
-    alignItems : "flex-end"
    }} >
     {
       [1,2,3,4,5,6,7,89,9].map((item) =>{
@@ -49,6 +24,67 @@ export default function Index() {
       })
     }
    </ScrollView>
+
+
+  {/* <View> */}
+    <FlatList
+    data={ [1,2,3,4,5,6,7,89,9,2,4,5,667,6765,453,3242,34314,1314]}
+    contentContainerStyle={{
+      backgroundColor : "yellow"
+    }}
+    //auto gride
+    numColumns={3}
+    //header comp
+    ListHeaderComponent={<View
+    style={{
+      backgroundColor : "orange",
+      height : 100,
+      marginBottom : 20
+
+    }}
+    ><Text>THis is header component of flatlist</Text></View>}
+    ListFooterComponent={
+<View
+    style={{
+      backgroundColor : "orange",
+      height : 100,
+      marginBottom : 20
+
+    }}
+    ><Text>THis is Footer component of flatlist</Text></View>
+
+    }
+    renderItem={(e) => {
+      console.log(e)
+      return(
+        <View
+        style={{
+          width :100,
+          height : 100,
+          backgroundColor : "blue",
+          marginBottom :20,
+          marginRight : 20,
+          borderRadius : 50,
+          borderWidth  :1,
+          justifyContent : "center",
+          alignItems : "center"
+        }}
+        >
+          <Text
+          style={{
+            fontWeight : 300,
+            fontSize : 50,
+            color : "white"
+          }}
+          >{e.item}</Text>
+          </View>
+      )
+    }}
+    // horizontal
+
+
+    />
+  {/* </View> */}
 
 
    </>
